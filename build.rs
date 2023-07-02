a32f388ed70a4a3a56faf4419f6c6821258da82b
ASleepyCat <dyeom340@gmail.com>
2023-07-03 00:06:40 +1000

    Fix build for publish

--- build.rs
+++ build.rs
@@ -1,13 +1,9 @@
-use std::{error::Error, env::set_current_dir, fs::create_dir_all, path::PathBuf};
+use std::{error::Error, env::set_current_dir};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    create_dir_all("src/go/out")?;
-    let out = PathBuf::from("src/go/out").canonicalize()?;
-
     set_current_dir("src/go")?;
     gobuild::Build::new()
         .file("./main.go")
-        .out_dir(out)
         .compile("get_version_from_module");
 
     Ok(())
