4440ca2f0f9d1733527bbcf10464e42a05f706e2
ASleepyCat <dyeom340@gmail.com>
2023-07-02 18:29:58 +1000

    Add some unit tests and fix regex matching for list-all

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ mod ffi;
 use clap::Parser;
 use dialoguer::{theme::ColorfulTheme, Select};
 use regex::Regex;
-use reqwest::blocking::{get, Response};
+use reqwest::blocking::Response;
 use semver::{Version, VersionReq};
 use std::{
     env::{self, consts},
     error::Error,
-    fs::{File, self},
-    io::{copy, Cursor},
+    fs::{self, File},
+    io::{self, Cursor},
     os::unix::prelude::PermissionsExt,
     path::PathBuf,
     str::FromStr,
@@ -46,7 +46,7 @@ fn find_program_path(program_name: &str) -> Option<PathBuf> {
 }
 
 fn get_http(url: &str) -> Result<Response, Box<dyn Error>> {
-    let response = get(url)?;
+    let response = reqwest::blocking::get(url)?;
     match response.error_for_status_ref() {
         Ok(_) => Ok(response),
         Err(e) => Err(Box::new(e)),
@@ -97,15 +97,21 @@ fn get_version_to_install(args: Args) -> Result<String, Box<dyn Error>> {
 }
 
 fn get_terraform_versions(args: Args, url: &str) -> Result<Vec<String>, Box<dyn Error>> {
-    let mut versions = vec![];
-
     let response = get_http(url)?;
     let contents = response.text()?;
-    let lines: Vec<_> = contents.split('\n').collect();
 
+    let versions = capture_terraform_versions(args, &contents);
+
+    Ok(versions)
+}
+
+fn capture_terraform_versions(args: Args, contents: &str) -> Vec<String> {
+    let mut versions = vec![];
+
+    let lines: Vec<_> = contents.split('\n').collect();
     // From https://github.com/warrensbox/terraform-switcher/blob/d7dfd1b44605b095937e94b981d24305b858ff8c/lib/list_versions.go#L28-L35
     let re = if args.list_all {
-        Regex::new(r#"/(\d+\.\d+\.\d+)(-[a-zA-z]+\d*)?/?""#).expect("Invalid regex")
+        Regex::new(r#"/(\d+\.\d+\.\d+)(?:-[a-zA-Z0-9-]+)?/?""#).expect("Invalid regex")
     } else {
         Regex::new(r#"/(\d+\.\d+\.\d+)/?""#).expect("Invalid regex")
     };
@@ -118,10 +124,10 @@ fn get_terraform_versions(args: Args, url: &str) -> Result<Vec<String>, Box<dyn 
         }
     }
 
-    Ok(versions)
+    versions
 }
 
-fn get_version_from_module(versions: &Vec<String>) -> Result<Option<String>, Box<dyn Error>> {
+fn get_version_from_module(versions: &[String]) -> Result<Option<String>, Box<dyn Error>> {
     let version_constraint = match ffi::get_version_from_module() {
         Some(constraint) => constraint,
         None => return Ok(None),
@@ -143,17 +149,17 @@ fn get_version_from_module(versions: &Vec<String>) -> Result<Option<String>, Box
 fn get_version_from_user_prompt(versions: &[String]) -> Result<String, Box<dyn Error>> {
     let version = prompt_version_to_user(versions)?;
 
-    Ok(version.to_owned())
+    Ok(version)
 }
 
-fn prompt_version_to_user(versions: &[String]) -> Result<&String, Box<dyn Error>> {
+fn prompt_version_to_user(versions: &[String]) -> Result<String, Box<dyn Error>> {
     println!("select a terraform version to install");
     let selection = Select::with_theme(&ColorfulTheme::default())
         .items(versions)
         .default(0)
         .interact()?;
 
-    Ok(&versions[selection])
+    Ok(versions[selection].to_owned())
 }
 
 fn install_version(program_path: PathBuf, version: &str) -> Result<(), Box<dyn Error>> {
@@ -170,7 +176,11 @@ fn install_version(program_path: PathBuf, version: &str) -> Result<(), Box<dyn E
     extract_zip_archive(&program_path, archive)
 }
 
-fn get_terraform_version_zip(version: &str, os: &str, arch: &str) -> Result<ZipArchive<Cursor<Vec<u8>>>, Box<dyn Error>> {
+fn get_terraform_version_zip(
+    version: &str,
+    os: &str,
+    arch: &str,
+) -> Result<ZipArchive<Cursor<Vec<u8>>>, Box<dyn Error>> {
     let zip_name = format!("terraform_{version}_{os}_{arch}.zip");
 
     if let Some(path) = home::home_dir().as_mut() {
@@ -188,7 +198,10 @@ fn get_terraform_version_zip(version: &str, os: &str, arch: &str) -> Result<ZipA
     download_and_save_terraform_version_zip(version, &zip_name)
 }
 
-fn download_and_save_terraform_version_zip(version: &str, zip_name: &str) -> Result<ZipArchive<Cursor<Vec<u8>>>, Box<dyn Error>> {
+fn download_and_save_terraform_version_zip(
+    version: &str,
+    zip_name: &str,
+) -> Result<ZipArchive<Cursor<Vec<u8>>>, Box<dyn Error>> {
     let url = format!("{ARCHIVE_URL}/{version}/{zip_name}");
     println!("downloading archive from {url}");
 
@@ -200,7 +213,7 @@ fn download_and_save_terraform_version_zip(version: &str, zip_name: &str) -> Res
             path.push(format!("{DEFAULT_LOCATION}/{zip_name}"));
             fs::write(path, &buffer)?;
         }
-        None => println!("unable to cache archive")
+        None => println!("unable to cache archive"),
     }
 
     let cursor = Cursor::new(buffer);
@@ -222,8 +235,150 @@ fn extract_zip_archive(
     outfile.set_permissions(perms)?;
 
     // Write the contents of the file to the output file
-    copy(&mut file, &mut outfile)?;
+    io::copy(&mut file, &mut outfile)?;
 
     println!("extracted archive to {program_path:?}");
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{io::Write, path::Path};
+    use tempdir::TempDir;
+
+    const LINES: &str = "<html><head>
+        <title>Terraform Versions | HashiCorp Releases</title>
+
+    </head>
+    <body>
+        <ul>
+            <li>
+            <a href=\"../\">../</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.3.0/\">terraform_1.3.0</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.3.0-rc1/\">terraform_1.3.0-rc1</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.3.0-beta1/\">terraform_1.3.0-beta1</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.3.0-alpha20220608/\">terraform_1.3.0-alpha20220608</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.2.0/\">terraform_1.2.0</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.2.0-rc1/\">terraform_1.2.0-rc1</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.2.0-beta1/\">terraform_1.2.0-beta1</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.2.0-alpha20220413/\">terraform_1.2.0-alpha20220413</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.2.0-alpha-20220328/\">terraform_1.2.0-alpha-20220328</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.1.0/\">terraform_1.1.0</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.1.0-rc1/\">terraform_1.1.0-rc1</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.1.0-beta1/\">terraform_1.1.0-beta1</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.1.0-alpha20211029/\">terraform_1.1.0-alpha20211029</a>
+            </li>
+            <li>
+            <a href=\"/terraform/1.0.0/\">terraform_1.0.0</a>
+            </li>
+            <li>
+            <a href=\"/terraform/0.15.0/\">terraform_0.15.0</a>
+            </li>
+            <li>
+            <a href=\"/terraform/0.15.0-rc1/\">terraform_0.15.0-rc1</a>
+            </li>
+            <li>
+            <a href=\"/terraform/0.15.0-beta1/\">terraform_0.15.0-beta1</a>
+            </li>
+            <li>
+            <a href=\"/terraform/0.15.0-alpha20210107/\">terraform_0.15.0-alpha20210107</a>
+            </li>
+            
+        </ul>
+
+</body></html>";
+
+    #[test]
+    fn test_capture_terraform_versions() -> Result<(), Box<dyn Error>> {
+        let expected_versions = vec!["1.3.0", "1.2.0", "1.1.0", "1.0.0", "0.15.0"];
+        let args = Args {
+            list_all: false,
+            version: None,
+        };
+        let actual_versions = capture_terraform_versions(args, LINES);
+
+        assert_eq!(expected_versions, actual_versions);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_capture_terraform_versions_list_all() -> Result<(), Box<dyn Error>> {
+        let expected_versions = vec![
+            "1.3.0",
+            "1.3.0-rc1",
+            "1.3.0-beta1",
+            "1.3.0-alpha20220608",
+            "1.2.0",
+            "1.2.0-rc1",
+            "1.2.0-beta1",
+            "1.2.0-alpha20220413",
+            "1.2.0-alpha-20220328",
+            "1.1.0",
+            "1.1.0-rc1",
+            "1.1.0-beta1",
+            "1.1.0-alpha20211029",
+            "1.0.0",
+            "0.15.0",
+            "0.15.0-rc1",
+            "0.15.0-beta1",
+            "0.15.0-alpha20210107",
+        ];
+        let args = Args {
+            list_all: true,
+            version: None,
+        };
+        let actual_versions = capture_terraform_versions(args, LINES);
+
+        assert_eq!(expected_versions, actual_versions);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_get_version_from_module() -> Result<(), Box<dyn Error>> {
+        const EXPECTED_VERSION: &str = "1.0.0";
+        let versions: Vec<String> = vec![EXPECTED_VERSION.to_string()];
+
+        let tmp_dir = TempDir::new("test_get_version_from_module")?;
+        let file_path = tmp_dir.path().join("version.tf");
+        let mut file = File::create(&file_path)?;
+        file.write_all(b"terraform { required_version = \"1.0.0\" }")?;
+        env::set_current_dir(Path::new(&tmp_dir.path()))?;
+
+        let actual_version = get_version_from_module(&versions)?;
+        assert!(actual_version.is_some());
+        assert_eq!(EXPECTED_VERSION, actual_version.unwrap());
+
+        drop(file);
+        tmp_dir.close()?;
+        Ok(())
+    }
 }
