8a4dc6d9a1bc25b1f3462c2b97923d9551c6926e
ASleepyCat <dyeom340@gmail.com>
2023-07-03 20:03:20 +1000

    Revert setting /Werror on *nix to work around MSVC

--- build.rs
+++ build.rs
@@ -3,7 +3,6 @@ use std::{env::set_current_dir, error::Error};
 fn main() -> Result<(), Box<dyn Error>> {
     set_current_dir("src/go")?;
     gobuild::Build::new()
-        .warnings_into_errors(cfg!(unix))
         .file("./main.go")
         .compile("get_version_from_module");
 
